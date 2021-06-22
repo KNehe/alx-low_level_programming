9c6362218a955c75120390b2f6972a5ab4dcb067
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-06-22 11:22:30 +0000

    Add task 9 - learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return:  Always void
+*/
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+				continue;
+			}
+			prod = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (prod >= 10)
+			{
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			else
+			{			
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
