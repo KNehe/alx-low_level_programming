e205366e37ecbb26a87810f5c37cff30a4d451ad
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-06-22 07:07:06 +0000

    Add task 1 -I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+*/
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
