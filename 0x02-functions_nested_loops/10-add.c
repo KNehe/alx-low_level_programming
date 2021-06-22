1ec71dd3359970105583e16a40cd72b2c31b5c9c
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-06-22 12:43:00 +0000

    Add task 10 - a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ * @num1: first integer parameter
+ * @num2: second integer parameter
+ * Return: sum of num1 and num2
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
