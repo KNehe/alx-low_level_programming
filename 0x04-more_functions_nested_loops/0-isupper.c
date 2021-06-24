3ff5e23fa61d05e1b5fa46604a01e3b0504f52f6
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-06-24 04:46:28 +0000

    Add task 0 - isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: Character to be checked
+ * Return: 1 if uppercase; otherwise 0
+*/
+
+int _isupper(int c)
+{
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
+}
