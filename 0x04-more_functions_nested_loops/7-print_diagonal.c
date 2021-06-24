63efae1cc11b00642a261dda4616cf46d102630d
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-06-24 06:52:48 +0000

    Add task 6 - the shortest distance between two points is a straight line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Draws a diagonal line on the terminal
+ * should end with a \n
+ * @n: Number od times the character \ should be printed
+ * Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n ; i++)
+		{
+			for (space = 1; space <= n ; space++)
+			{
+			_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
