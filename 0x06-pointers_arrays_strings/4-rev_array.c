e4199594e4c09a3b9992ef3e0cf54c6986347a56
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-06-30 15:42:02 +0000

    Add task 4 - paranoid in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * reverse_array - Compares two strings
+ * @a: Pointer to array of integers
+ * @n: Number of elements of a
+ * Return: void
+*/
+void  reverse_array(int *a, int n)
+{
+	int i, *startPt, *endPoint, temp;
+
+	startPt = a;
+	endPoint = (a + n - 1);
+	for (i = 1; i < n / 2; i++)
+	{
+		temp = *startPt;
+		*startPt = *endPoint;
+		*endPoint = temp;
+		startPt++;
+		endPoint--;
+	}
+}
