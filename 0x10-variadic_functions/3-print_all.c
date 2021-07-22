66f2360857b225de8afca562be540f0ff710bdc2
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-07-22 10:56:51 +0000

    Add task 3 - print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,66 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * str_helper - Prints string
+  * @str: Pointer to string
+  * Return: void
+  */
+void str_helper(char *str)
+{
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+/**
+  * print_all - Prints anything
+  * @...: Unlimited args
+  * @format: String format
+  * Return: void
+  */
+void print_all(const char * const format, ...)
+{
+	const char *s;
+	va_list args;
+	int c, i;
+	float f;
+	char *str;
+
+	s = format;
+	va_start(args, format);
+	while (s && *s != '\0')
+	{
+		switch (*s)
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case  'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				str_helper(str);
+				break;
+			default:
+				break;
+		}
+		s++;
+		if (s && *s != '\0' && (*s == 'c' || *s == 'i' || *s == 'f' || *s == 's'))
+			printf(", ");
+	}
+	printf("\n");
+	va_end(args);
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,27 @@
 #ifndef VAR_FUNC
 #define VAR_FUNC
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+void str_helper(char *str);
+
+
+/**
+  * struct custom - struct
+  * @letter: char
+  * @func: Function pointer
+  */
+typedef struct custom
+{
+	char letter;
+	void (*func)(va_list);
+} mytype;
 
 #endif
